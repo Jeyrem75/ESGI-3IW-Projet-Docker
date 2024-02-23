2e4d04de8c16a211ec6421c742092f2cf827fefd
Jeyrem75 <ly.jerem@gmail.com>
2024-02-23 17:24:00 +0100

    fixed permissions with chown and chmod in init.sh

--- TP-Final-3IW-ESGI/init.sh
+++ TP-Final-3IW-ESGI/init.sh
@@ -4,6 +4,9 @@ while ! nc -z db 3306; do
     sleep 1
 done
 
+chown -R www-data:www-data /var/www/html/storage/
+chmod +x /var/www/html/storage/
+
 php artisan key:generate
 php artisan migrate:fresh --seed
 
